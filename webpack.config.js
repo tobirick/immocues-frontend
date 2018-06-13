c84617dcbad7208de00428e10b16ce2f6c83b50b
tobirick <tobirickmann@gmail.com>
2018-06-13 16:50:14 +0200

    add settings for heroku build

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,57 @@
-const path = require('path');
-const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-require('dotenv').config();
+const path = require("path");
+const webpack = require("webpack");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-const CSSExtract = new ExtractTextPlugin('styles.css');
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
-return (module.exports = {
-   entry: './src/app.js',
-   output: {
-      path: path.join(__dirname, 'public', 'dist'),
-      filename: 'bundle.js'
-   },
-   module: {
+if (process.env.NODE_ENV === "development") {
+  require("dotenv").config({ path: ".env.development" });
+}
+
+module.exports = env => {
+  const isProduction = env === "production";
+  const CSSExtract = new ExtractTextPlugin("styles.css");
+
+  return {
+    entry: "./src/app.js",
+    output: {
+      path: path.join(__dirname, "public", "dist"),
+      filename: "bundle.js"
+    },
+    module: {
       rules: [
-         {
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-         },
-         {
-            test: /\.s?css$/,
-            use: CSSExtract.extract({
-               use: [
-                  {
-                     loader: 'css-loader',
-                     options: {
-                        sourceMap: false
-                     }
-                  },
-                  {
-                     loader: 'sass-loader',
-                     options: {
-                        sourceMap: true
-                     }
-                  }
-               ]
-            })
-         }
+        {
+          loader: "babel-loader",
+          test: /\.js$/,
+          exclude: /node_modules/
+        },
+        {
+          test: /\.s?css$/,
+          use: CSSExtract.extract({
+            use: [
+              {
+                loader: "css-loader",
+                options: {
+                  sourceMap: false
+                }
+              },
+              {
+                loader: "sass-loader",
+                options: {
+                  sourceMap: true
+                }
+              }
+            ]
+          })
+        }
       ]
-   },
-   plugins: [
-      CSSExtract
-   ],
-   devtool: 'inline-source-map',
-   devServer: {
-      contentBase: path.join(__dirname, 'public'),
+    },
+    plugins: [CSSExtract],
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, "public"),
       historyApiFallback: true,
-      publicPath: '/dist/'
-   }
-});
+      publicPath: "/dist/"
+    }
+  };
+};
