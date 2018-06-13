a8dc44ef11f96e6b4a9027723f4ae9f0dffb8869
tobirick <tobirickmann@gmail.com>
2018-06-13 19:16:14 +0200

    fix bug of unset lat and lng for google maps component on customer page

--- src/components/CustomerList/CustomerPreview.js
+++ src/components/CustomerList/CustomerPreview.js
@@ -1,102 +1,127 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import MapComponent from "../../helpers/maps/MapComponent";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import MapComponent from '../../helpers/maps/MapComponent';
 
 const CustomerPreview = ({ customer, setCustomer }) => {
-  return (
-    <div className="customer__preview">
-      <div className="customer__preview-actions">
-        <div className="customer__preview-button">
-          <Link to={`/customers/${customer.id}`}>
-            <i className="fas fa-arrow-right" />
-          </Link>
-        </div>
-        <div onClick={setCustomer(null)} className="customer__preview-button">
-          <i className="fal fa-times-circle" />
-        </div>
-      </div>
-      <div className="customer__preview-header">
-        <div className="customer__preview-header-left">
-          <img
-            src={customer.profileImageSrc}
-            alt={`${customer.firstName} ${customer.lastName}`}
-          />
-        </div>
-        <div className="customer__preview-header-right">
-          <span className="customer__preview-header-name">
-            {customer.firstName} {customer.lastName}
-          </span>
-          <span className="customer__preview-header-title">
-            {customer.title ? customer.title : "---"}
-          </span>
-        </div>
-      </div>
-      <div className="customer__basic-info">
-        <div className="customer__basic-info-item">
-          <div className="customer__basic-info-item-label">Name</div>
-          <div className="customer__basic-info-item-text">
-            {customer.firstName} {customer.lastName}
-          </div>
-        </div>
+    return (
+        <div className="customer__preview">
+            <div className="customer__preview-actions">
+                <div className="customer__preview-button">
+                    <Link to={`/customers/${customer.id}`}>
+                        <i className="fas fa-arrow-right" />
+                    </Link>
+                </div>
+                <div
+                    onClick={setCustomer(null)}
+                    className="customer__preview-button"
+                >
+                    <i className="fal fa-times-circle" />
+                </div>
+            </div>
+            <div className="customer__preview-header">
+                <div className="customer__preview-header-left">
+                    <img
+                        src={customer.profileImageSrc}
+                        alt={`${customer.firstName} ${customer.lastName}`}
+                    />
+                </div>
+                <div className="customer__preview-header-right">
+                    <span className="customer__preview-header-name">
+                        {customer.firstName} {customer.lastName}
+                    </span>
+                    <span className="customer__preview-header-title">
+                        {customer.title ? customer.title : '---'}
+                    </span>
+                </div>
+            </div>
+            <div className="customer__basic-info">
+                <div className="customer__basic-info-item">
+                    <div className="customer__basic-info-item-label">Name</div>
+                    <div className="customer__basic-info-item-text">
+                        {customer.firstName} {customer.lastName}
+                    </div>
+                </div>
 
-        <div className="customer__basic-info-item">
-          <div className="customer__basic-info-item-label">Title</div>
-          <div className="customer__basic-info-item-text">
-            {customer.title ? customer.title : "---"}
-          </div>
-        </div>
+                <div className="customer__basic-info-item">
+                    <div className="customer__basic-info-item-label">Title</div>
+                    <div className="customer__basic-info-item-text">
+                        {customer.title ? customer.title : '---'}
+                    </div>
+                </div>
 
-        <div className="customer__basic-info-item">
-          <div className="customer__basic-info-item-label">Company</div>
-          <div className="customer__basic-info-item-text">
-            {customer.company ? customer.company : "---"}
-          </div>
-        </div>
+                <div className="customer__basic-info-item">
+                    <div className="customer__basic-info-item-label">
+                        Company
+                    </div>
+                    <div className="customer__basic-info-item-text">
+                        {customer.company ? customer.company : '---'}
+                    </div>
+                </div>
 
-        <div className="customer__basic-info-item">
-          <div className="customer__basic-info-item-label">E-Mail</div>
-          <div className="customer__basic-info-item-text">
-            {customer.eMail ? customer.eMail : "---"}
-          </div>
-        </div>
+                <div className="customer__basic-info-item">
+                    <div className="customer__basic-info-item-label">
+                        E-Mail
+                    </div>
+                    <div className="customer__basic-info-item-text">
+                        {customer.eMail ? customer.eMail : '---'}
+                    </div>
+                </div>
 
-        <div className="customer__basic-info-item">
-          <div className="customer__basic-info-item-label">Phone Number</div>
-          <div className="customer__basic-info-item-text">
-            {customer.phoneNumber ? customer.phoneNumber : "---"}
-          </div>
-        </div>
+                <div className="customer__basic-info-item">
+                    <div className="customer__basic-info-item-label">
+                        Phone Number
+                    </div>
+                    <div className="customer__basic-info-item-text">
+                        {customer.phoneNumber ? customer.phoneNumber : '---'}
+                    </div>
+                </div>
 
-        <div className="customer__basic-info-item">
-          <div className="customer__basic-info-item-label">
-            Mobile Phone Number
-          </div>
-          <div className="customer__basic-info-item-text">
-            {customer.mobilePhoneNumber ? customer.mobilePhoneNumber : "---"}
-          </div>
-        </div>
+                <div className="customer__basic-info-item">
+                    <div className="customer__basic-info-item-label">
+                        Mobile Phone Number
+                    </div>
+                    <div className="customer__basic-info-item-text">
+                        {customer.mobilePhoneNumber
+                            ? customer.mobilePhoneNumber
+                            : '---'}
+                    </div>
+                </div>
 
-        <div className="customer__basic-info-item">
-          <div className="customer__basic-info-item-label">Website</div>
-          <div className="customer__basic-info-item-text">
-            {customer.websiteUrl ? customer.websiteUrl : "---"}
-          </div>
-        </div>
+                <div className="customer__basic-info-item">
+                    <div className="customer__basic-info-item-label">
+                        Website
+                    </div>
+                    <div className="customer__basic-info-item-text">
+                        {customer.websiteUrl ? customer.websiteUrl : '---'}
+                    </div>
+                </div>
 
-        <div className="customer__basic-info-item">
-          <div className="customer__basic-info-item-label">Address</div>
-          <MapComponent
-            lat={customer.addressLatLng.lat}
-            lng={customer.addressLatLng.lng}
-          />
-          <div className="customer__basic-info-item-text">
-            <strong>{customer.addressStreet}</strong>
-            <p>{customer.addressCity}</p>
-          </div>
+                <div className="customer__basic-info-item">
+                    <div className="customer__basic-info-item-label">
+                        Address
+                    </div>
+                    {customer.addressLatLng && (
+                        <MapComponent
+                            lat={customer.addressLatLng.lat}
+                            lng={customer.addressLatLng.lng}
+                        />
+                    )}
+                    <div className="customer__basic-info-item-text">
+                        <strong>
+                            {customer.addressStreet
+                                ? customer.addressStreet
+                                : '---'}
+                        </strong>
+                        <p>
+                            {customer.addressCity
+                                ? customer.addressCity
+                                : '---'}
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default CustomerPreview;
--- src/components/CustomerPage/CustomerPageSidebar/CustomerPageLeftSidebar.js
+++ src/components/CustomerPage/CustomerPageSidebar/CustomerPageLeftSidebar.js
@@ -1,96 +1,118 @@
-import React from "react";
-import MapComponent from "../../../helpers/maps/MapComponent";
+import React from 'react';
+import MapComponent from '../../../helpers/maps/MapComponent';
 
 const CustomerPageLeftSidebar = ({ customer }) => {
-  return (
-    <div>
-      <div className="customer__header-info">
-        <div className="customer__header-info-left">
-          <img
-            className="customer__header-info-image"
-            src={customer.profileImageSrc}
-            alt={`${customer.firstName} ${customer.lastName}`}
-          />
-        </div>
-        <div className="customer__header-info-center">
-          <span className="customer__header-info-name">
-            {customer.firstName} {customer.lastName}
-          </span>
-          <span className="customer__header-info-title">
-            {customer.title ? customer.title : "---"}
-          </span>
-        </div>
-        <div className="customer__header-info-right">
-          <i className="far fa-star" />
-          <i className="far fa-ellipsis-v" />
-        </div>
-      </div>
-      <div className="customer__basic-info">
-        <div className="customer__basic-info-item">
-          <div className="customer__basic-info-item-label">Name</div>
-          <div className="customer__basic-info-item-text">
-            {customer.firstName} {customer.lastName}
-          </div>
-        </div>
+    return (
+        <div>
+            <div className="customer__header-info">
+                <div className="customer__header-info-left">
+                    <img
+                        className="customer__header-info-image"
+                        src={customer.profileImageSrc}
+                        alt={`${customer.firstName} ${customer.lastName}`}
+                    />
+                </div>
+                <div className="customer__header-info-center">
+                    <span className="customer__header-info-name">
+                        {customer.firstName} {customer.lastName}
+                    </span>
+                    <span className="customer__header-info-title">
+                        {customer.title ? customer.title : '---'}
+                    </span>
+                </div>
+                <div className="customer__header-info-right">
+                    <i className="far fa-star" />
+                    <i className="far fa-ellipsis-v" />
+                </div>
+            </div>
+            <div className="customer__basic-info">
+                <div className="customer__basic-info-item">
+                    <div className="customer__basic-info-item-label">Name</div>
+                    <div className="customer__basic-info-item-text">
+                        {customer.firstName} {customer.lastName}
+                    </div>
+                </div>
 
-        <div className="customer__basic-info-item">
-          <div className="customer__basic-info-item-label">Title</div>
-          <div className="customer__basic-info-item-text">
-            {customer.title ? customer.title : "---"}
-          </div>
-        </div>
+                <div className="customer__basic-info-item">
+                    <div className="customer__basic-info-item-label">Title</div>
+                    <div className="customer__basic-info-item-text">
+                        {customer.title ? customer.title : '---'}
+                    </div>
+                </div>
 
-        <div className="customer__basic-info-item">
-          <div className="customer__basic-info-item-label">Company</div>
-          <div className="customer__basic-info-item-text">
-            {customer.company ? customer.company : "---"}
-          </div>
-        </div>
+                <div className="customer__basic-info-item">
+                    <div className="customer__basic-info-item-label">
+                        Company
+                    </div>
+                    <div className="customer__basic-info-item-text">
+                        {customer.company ? customer.company : '---'}
+                    </div>
+                </div>
 
-        <div className="customer__basic-info-item">
-          <div className="customer__basic-info-item-label">E-Mail</div>
-          <div className="customer__basic-info-item-text">
-            {customer.eMail ? customer.eMail : "---"}
-          </div>
-        </div>
+                <div className="customer__basic-info-item">
+                    <div className="customer__basic-info-item-label">
+                        E-Mail
+                    </div>
+                    <div className="customer__basic-info-item-text">
+                        {customer.eMail ? customer.eMail : '---'}
+                    </div>
+                </div>
 
-        <div className="customer__basic-info-item">
-          <div className="customer__basic-info-item-label">Phone Number</div>
-          <div className="customer__basic-info-item-text">
-            {customer.phoneNumber ? customer.phoneNumber : "---"}
-          </div>
-        </div>
+                <div className="customer__basic-info-item">
+                    <div className="customer__basic-info-item-label">
+                        Phone Number
+                    </div>
+                    <div className="customer__basic-info-item-text">
+                        {customer.phoneNumber ? customer.phoneNumber : '---'}
+                    </div>
+                </div>
 
-        <div className="customer__basic-info-item">
-          <div className="customer__basic-info-item-label">
-            Mobile Phone Number
-          </div>
-          <div className="customer__basic-info-item-text">
-            {customer.mobilePhoneNumber ? customer.mobilePhoneNumber : "---"}
-          </div>
-        </div>
+                <div className="customer__basic-info-item">
+                    <div className="customer__basic-info-item-label">
+                        Mobile Phone Number
+                    </div>
+                    <div className="customer__basic-info-item-text">
+                        {customer.mobilePhoneNumber
+                            ? customer.mobilePhoneNumber
+                            : '---'}
+                    </div>
+                </div>
 
-        <div className="customer__basic-info-item">
-          <div className="customer__basic-info-item-label">Website</div>
-          <div className="customer__basic-info-item-text">
-            {customer.websiteUrl ? customer.websiteUrl : "---"}
-          </div>
-        </div>
+                <div className="customer__basic-info-item">
+                    <div className="customer__basic-info-item-label">
+                        Website
+                    </div>
+                    <div className="customer__basic-info-item-text">
+                        {customer.websiteUrl ? customer.websiteUrl : '---'}
+                    </div>
+                </div>
 
-        <div className="customer__basic-info-item">
-          <div className="customer__basic-info-item-label">Address</div>
-          <MapComponent
-            lat={customer.addressLatLng.lat}
-            lng={customer.addressLatLng.lng}
-          />
-          <div className="customer__basic-info-item-text">
-            <strong>{customer.addressStreet}</strong>
-            <p>{customer.addressCity}</p>
-          </div>
+                <div className="customer__basic-info-item">
+                    <div className="customer__basic-info-item-label">
+                        Address
+                    </div>
+                    {customer.addressLatLng && (
+                        <MapComponent
+                            lat={customer.addressLatLng.lat}
+                            lng={customer.addressLatLng.lng}
+                        />
+                    )}
+                    <div className="customer__basic-info-item-text">
+                        <strong>
+                            {customer.addressStreet
+                                ? customer.addressStreet
+                                : '---'}
+                        </strong>
+                        <p>
+                            {customer.addressCity
+                                ? customer.addressCity
+                                : '---'}
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default CustomerPageLeftSidebar;
